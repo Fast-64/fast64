8393e87bd453f723919e5a8560668c2cf17f373d
Yanis42 <35189056+Yanis42@users.noreply.github.com>
2022-03-07 23:00:29 +0100

    [OoT] Fixed Scene Export and removed unwanted changes in ``spec`` (#79)
    * fix part 1
    
    * fix part 2, add a C comment when adding scenes
    
    * Fixed ``Remove Scene`` option, removed unwanted changes in spec
    
    * removed unused (?) code in OOT_SearchSceneEnumOperator
    
    * Fixed C comments
    
    * Review
    
    * Missed review stuff
    
    * is none
    
    * Fixed multi-file export typo for cutscenes
    
    * small suggestion
    
    * impromptu spaces indent
    
    * Fixed ``ootSceneIDToName`` and a bug when removing scenes
    
    * Review 2
    
    * Review 3
    
    * Bugfixes
    
    * Bugfix & minor improvements
    
    * Review and black formatting (1 file)

--- fast64_internal/oot/c_writer/__init__.py
+++ fast64_internal/oot/c_writer/__init__.py
@@ -1,6 +1,4 @@
 from .oot_level_c import *
-from .oot_scene_id_c import *
 from .oot_scene_table_c import *
-from .oot_segment_symbols_c import *
 from .oot_spec import *
 from .oot_scene_folder import *
