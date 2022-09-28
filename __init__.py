9c4ff9ddb26b05439493d483491bf179e79cc039
Dragorn421 <Dragorn421@users.noreply.github.com>
2022-09-28 20:13:12 +0200

    Bump version to 2.0.0 (#146)

--- __init__.py
+++ __init__.py
@@ -24,7 +24,7 @@ from . import addon_updater_ops
 # info about add on
 bl_info = {
     "name": "Fast64",
-    "version": (1, 0, 1),
+    "version": (2, 0, 0),
     "author": "kurethedead",
     "location": "3DView",
     "description": "Plugin for exporting F3D display lists and other game data related to Super Mario 64.",
