2a84b78bacebddedf07e9482c81f0e9f70e4b63a
scut <scut>
2022-11-11 09:18:41 -0500

    removed KCS stuff

--- __init__.py
+++ __init__.py
@@ -16,8 +16,6 @@ from .fast64_internal.oot import OOT_Properties, oot_register, oot_unregister
 from .fast64_internal.oot.oot_level import OOT_ObjectProperties
 from .fast64_internal.utility_anim import utility_anim_register, utility_anim_unregister, ArmatureApplyWithMeshOperator
 
-from .fast64_internal.kcs import kcs_register, kcs_unregister
-
 from .fast64_internal.f3d.f3d_material import mat_register, mat_unregister
 from .fast64_internal.f3d.f3d_render_engine import render_engine_register, render_engine_unregister
 from .fast64_internal.f3d.f3d_writer import f3d_writer_register, f3d_writer_unregister
@@ -500,7 +498,6 @@ def register():
     bsdf_conv_register()
     sm64_register(True)
     oot_register(True)
-    kcs_register()
 
     for cls in classes:
         register_recursive(cls)
@@ -544,7 +541,6 @@ def unregister():
     f3d_parser_unregister()
     sm64_unregister(True)
     oot_unregister(True)
-    kcs_unregister()
     mat_unregister()
     bsdf_conv_unregister()
     bsdf_conv_panel_unregsiter()
--- fast64_internal/__init__.py
+++ fast64_internal/__init__.py
@@ -1,6 +1,5 @@
 from .f3d_material_converter import *
 from .f3d import *
 from .sm64 import *
-from .kcs import *
 from .oot import *  # is this really needed?
 from .panels import *
