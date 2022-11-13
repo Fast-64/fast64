0ff0d493d0a44c9f0c48e21a3ed9399ba18d0161
Yanis42 <35189056+Yanis42@users.noreply.github.com>
2022-11-13 12:48:55 +0100

    fixes

--- fast64_internal/oot/oot_f3d_writer.py
+++ fast64_internal/oot/oot_f3d_writer.py
@@ -63,36 +63,7 @@ def getColliderMat(name: str, color: tuple[float, float, float, float]) -> bpy.t
         return bpy.data.materials[name]
 
 
-class OOTDLExportSettings(bpy.types.PropertyGroup):
-    name: bpy.props.StringProperty(name="DL Name", default="gBoulderFragmentsDL")
-    folder: bpy.props.StringProperty(name="DL Folder", default="gameplay_keep")
-    customPath: bpy.props.StringProperty(name="Custom DL Path", subtype="FILE_PATH")
-    isCustom: bpy.props.BoolProperty(name="Use Custom Path")
-    removeVanillaData: bpy.props.BoolProperty(name="Replace Vanilla DLs")
-    drawLayer: bpy.props.EnumProperty(name="Draw Layer", items=ootEnumDrawLayers)
-    actorOverlayName: bpy.props.StringProperty(name="Overlay", default="")
-    flipbookUses2DArray: bpy.props.BoolProperty(name="Has 2D Flipbook Array", default=False)
-    flipbookArrayIndex2D: bpy.props.IntProperty(name="Index if 2D Array", default=0, min=0)
-    customAssetIncludeDir: bpy.props.StringProperty(
-        name="Asset Include Directory",
-        default="assets/objects/gameplay_keep",
-        description="Used in #include for including image files",
-    )
 
-
-class OOTDLImportSettings(bpy.types.PropertyGroup):
-    name: bpy.props.StringProperty(name="DL Name", default="gBoulderFragmentsDL")
-    folder: bpy.props.StringProperty(name="DL Folder", default="gameplay_keep")
-    customPath: bpy.props.StringProperty(name="Custom DL Path", subtype="FILE_PATH")
-    isCustom: bpy.props.BoolProperty(name="Use Custom Path")
-    removeDoubles: bpy.props.BoolProperty(name="Remove Doubles", default=True)
-    importNormals: bpy.props.BoolProperty(name="Import Normals", default=True)
-    drawLayer: bpy.props.EnumProperty(name="Draw Layer", items=ootEnumDrawLayers)
-    actorOverlayName: bpy.props.StringProperty(name="Overlay", default="")
-    flipbookUses2DArray: bpy.props.BoolProperty(name="Has 2D Flipbook Array", default=False)
-    flipbookArrayIndex2D: bpy.props.IntProperty(name="Index if 2D Array", default=0, min=0)
-    autoDetectActorScale: bpy.props.BoolProperty(name="Auto Detect Actor Scale", default=True)
-    actorScale: bpy.props.FloatProperty(name="Actor Scale", min=0, default=100)
 
 
 # returns:
--- fast64_internal/oot/other/panel/viewport/display_list/classes.py
+++ fast64_internal/oot/other/panel/viewport/display_list/classes.py
@@ -1,6 +1,23 @@
 from bpy.types import PropertyGroup
-from bpy.props import StringProperty, BoolProperty, EnumProperty
+from bpy.props import StringProperty, BoolProperty, EnumProperty, IntProperty, FloatProperty
 from ......f3d.f3d_material import ootEnumDrawLayers
+
+
+class OOTDLExportSettings(PropertyGroup):
+    name: StringProperty(name="DL Name", default="gBoulderFragmentsDL")
+    folder: StringProperty(name="DL Folder", default="gameplay_keep")
+    customPath: StringProperty(name="Custom DL Path", subtype="FILE_PATH")
+    isCustom: BoolProperty(name="Use Custom Path")
+    removeVanillaData: BoolProperty(name="Replace Vanilla DLs")
+    drawLayer: EnumProperty(name="Draw Layer", items=ootEnumDrawLayers)
+    actorOverlayName: StringProperty(name="Overlay", default="")
+    flipbookUses2DArray: BoolProperty(name="Has 2D Flipbook Array", default=False)
+    flipbookArrayIndex2D: IntProperty(name="Index if 2D Array", default=0, min=0)
+    customAssetIncludeDir: StringProperty(
+        name="Asset Include Directory",
+        default="assets/objects/gameplay_keep",
+        description="Used in #include for including image files",
+    )
 
 
 class OOTDLImportSettings(PropertyGroup):
@@ -11,17 +28,8 @@ class OOTDLImportSettings(PropertyGroup):
     removeDoubles: BoolProperty(name="Remove Doubles", default=True)
     importNormals: BoolProperty(name="Import Normals", default=True)
     drawLayer: EnumProperty(name="Draw Layer", items=ootEnumDrawLayers)
-
-
-class OOTDLExportSettings(PropertyGroup):
-    name: StringProperty(name="DL Name", default="gBoulderFragmentsDL")
-    folder: StringProperty(name="DL Folder", default="gameplay_keep")
-    customPath: StringProperty(name="Custom DL Path", subtype="FILE_PATH")
-    isCustom: BoolProperty(name="Use Custom Path")
-    removeVanillaData: BoolProperty(name="Replace Vanilla DLs")
-    drawLayer: EnumProperty(name="Draw Layer", items=ootEnumDrawLayers)
-    customAssetIncludeDir: StringProperty(
-        name="Asset Include Directory",
-        default="assets/objects/gameplay_keep",
-        description="Used in #include for including image files",
-    )
+    actorOverlayName: StringProperty(name="Overlay", default="")
+    flipbookUses2DArray: BoolProperty(name="Has 2D Flipbook Array", default=False)
+    flipbookArrayIndex2D: IntProperty(name="Index if 2D Array", default=0, min=0)
+    autoDetectActorScale: BoolProperty(name="Auto Detect Actor Scale", default=True)
+    actorScale: FloatProperty(name="Actor Scale", min=0, default=100)
