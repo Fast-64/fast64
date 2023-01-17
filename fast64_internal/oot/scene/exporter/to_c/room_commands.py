0363b61e5e5289bdf3844b552fc7344ee7c383ee
Yanis42 <35189056+Yanis42@users.noreply.github.com>
2023-01-17 20:36:28 +0100

    forgot the comma

--- fast64_internal/oot/scene/exporter/to_c/room_commands.py
+++ fast64_internal/oot/scene/exporter/to_c/room_commands.py
@@ -31,7 +31,7 @@ def getTimeSettingsCmd(outRoom: OOTRoom):
 def getWindSettingsCmd(outRoom: OOTRoom):
     return (
         indent
-        + f"SCENE_CMD_WIND_SETTINGS({', '.join(f'{dir}' for dir in outRoom.windVector)}, {outRoom.windStrength})\n"
+        + f"SCENE_CMD_WIND_SETTINGS({', '.join(f'{dir}' for dir in outRoom.windVector)}, {outRoom.windStrength}),\n"
     )
 
 
