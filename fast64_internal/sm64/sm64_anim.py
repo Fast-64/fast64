1641160d4a8c6ced315fe67d9844059b70d80bed
Lilaa3 <87947656+Lilaa3@users.noreply.github.com>
2023-06-14 14:32:22 +0100

    Updated mario animation names and added automatic stashing (#231)
    * Updated mario animation names and added automatic stashing
    
    * Breakdamce
    
    * Fixed issues pointed out by thecozies

--- fast64_internal/sm64/sm64_anim.py
+++ fast64_internal/sm64/sm64_anim.py
@@ -33,6 +33,7 @@ from ..utility import (
     decompFolderMessage,
     makeWriteInfoBox,
     writeBoxExportType,
+    stashActionInArmature,
     enumExportHeaderType,
 )
 
@@ -348,7 +349,10 @@ def exportAnimationInsertableBinary(filepath, armatureObj, isDMA, loopAnim):
 def exportAnimationCommon(armatureObj, loopAnim, name):
     if armatureObj.animation_data is None or armatureObj.animation_data.action is None:
         raise PluginError("No active animation selected.")
+
     anim = armatureObj.animation_data.action
+    stashActionInArmature(armatureObj, anim)
+
     sm64_anim = SM64_Animation(toAlnum(name + "_" + anim.name))
 
     nodeCount = len(armatureObj.data.bones)
@@ -544,8 +548,9 @@ def importAnimationToBlender(romfile, startAddress, armatureObj, segmentData, is
 
     if armatureObj.animation_data is None:
         armatureObj.animation_data_create()
-    armatureObj.animation_data.action = anim
 
+    stashActionInArmature(armatureObj, anim)
+    armatureObj.animation_data.action = anim
 
 def readAnimation(name, romfile, startAddress, segmentData, isDMA):
     animationHeader = readAnimHeader(name, romfile, startAddress, segmentData, isDMA)
