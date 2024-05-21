3d031000a0f3428739d3a0bfbce282ab52011d81
Lila <87947656+Lilaa3@users.noreply.github.com>
2024-05-21 13:49:59 +0100

    Remove boxing

--- __init__.py
+++ __init__.py
@@ -123,7 +123,7 @@ class Fast64_GlobalSettingsPanel(bpy.types.Panel):
             if fast64_settings.auto_pick_texture_format:
                 col.prop(fast64_settings, "prefer_rgba_over_ci")
 
-        draw_repo_settings(col.box(), context)
+        draw_repo_settings(col, context)
 
 
 class Fast64_GlobalToolsPanel(bpy.types.Panel):
