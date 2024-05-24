04342993d537e314f935c52dc1cdb88e3abed53a
Lila <lilalovesall@gmail.com>
2024-05-24 21:43:11 +0100

    [SM64] Move into the new folders for settings rework (#324)
    * Moved tools and settings into their own folders
    
    * remove more imports
    
    * forgot to move this
    
    * format

---
+++ fast64_internal/sm64/settings/constants.py
@@ -0,0 +1,39 @@
+sm64GoalTypeEnum = [
+    ("All", "All", "All"),
+    ("Export Object/Actor/Anim", "Export Object/Actor/Anim", "Export Object/Actor/Anim"),
+    ("Export Level", "Export Level", "Export Level"),
+    ("Export Displaylist", "Export Displaylist", "Export Displaylist"),
+    ("Export UI Image", "Export UI Image", "Export UI Image"),
+]
+
+enumExportType = [
+    ("C", "C", "C"),
+    ("Binary", "Binary", "Binary"),
+    ("Insertable Binary", "Insertable Binary", "Insertable Binary"),
+]
+
+enumCompressionFormat = [
+    ("mio0", "MIO0", "MIO0"),
+    ("yay0", "YAY0", "YAY0"),
+]
+
+enumRefreshVer = [
+    ("Refresh 3", "Refresh 3", "Refresh 3"),
+    ("Refresh 4", "Refresh 4", "Refresh 4"),
+    ("Refresh 5", "Refresh 5", "Refresh 5"),
+    ("Refresh 6", "Refresh 6", "Refresh 6"),
+    ("Refresh 7", "Refresh 7", "Refresh 7"),
+    ("Refresh 8", "Refresh 8", "Refresh 8"),
+    ("Refresh 10", "Refresh 10", "Refresh 10"),
+    ("Refresh 11", "Refresh 11", "Refresh 11"),
+    ("Refresh 12", "Refresh 12", "Refresh 12"),
+    ("Refresh 13", "Refresh 13", "Refresh 13"),
+]
+
+draw_layer_enums = [
+    ("1", "Solid", "0x01"),
+    ("2", "Solid Decal", "0x02"),
+    ("4", "Transparent (No Blending)", "0x04"),
+    ("5", "Transparent (Blending Front)", "0x05"),
+    ("6", "Transparent (Blending Back)", "0x06"),
+]
--- fast64_internal/utility.py
+++ fast64_internal/utility.py
@@ -33,21 +33,10 @@ axis_enums = [
     ("-Y", "-Y", "-Y"),
 ]
 
-enumExportType = [
-    ("C", "C", "C"),
-    ("Binary", "Binary", "Binary"),
-    ("Insertable Binary", "Insertable Binary", "Insertable Binary"),
-]
-
 enumExportHeaderType = [
     # ('None', 'None', 'Headers are not written'),
     ("Actor", "Actor Data", "Headers are written to a group in actors/"),
     ("Level", "Level Data", "Headers are written to a specific level in levels/"),
-]
-
-enumCompressionFormat = [
-    ("mio0", "MIO0", "MIO0"),
-    ("yay0", "YAY0", "YAY0"),
 ]
 
 
