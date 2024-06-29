84f0921baa448821ee9e27d20769ea5e5241eaac
Lila <lilalovesall@gmail.com>
2024-06-29 22:42:58 +0100

    Node tree copy

--- fast64_internal/f3d/f3d_gltf.py
+++ fast64_internal/f3d/f3d_gltf.py
@@ -1,9 +1,15 @@
 import bpy
-from bpy.types import Image
+from bpy.types import Image, NodeTree
 
 from ..gltf_utility import GlTF2SubExtension
 from .f3d_gbi import F3D, get_F3D_GBI
-from .f3d_material import all_combiner_uses, F3DMaterialProperty, TextureProperty
+from .f3d_material import (
+    all_combiner_uses,
+    createScenePropertiesForMaterial,
+    link_f3d_material_library,
+    F3DMaterialProperty,
+    TextureProperty,
+)
 
 # TODO: Check glTF addon version instead
 if bpy.app.version >= (3, 6, 0):
@@ -30,6 +36,58 @@ else:
 from io_scene_gltf2.io.com import gltf2_io
 from io_scene_gltf2.blender.imp.gltf2_blender_image import BlenderImage
 from io_scene_gltf2.io.com.gltf2_io_constants import TextureFilter, TextureWrap
+
+EXCLUDE_FROM_NODE = (
+    "rna_type",
+    "type",
+    "dimensions",
+    "inputs",
+    "interface",
+    "outputs",
+    "internal_links",
+    "texture_mapping",
+    "color_mapping",
+    "image_user",
+)
+INCLUDE_FROM_INPUT_OUTPUT = ("default_value", "name")
+
+
+def node_tree_copy(src: NodeTree, dst: NodeTree):
+    def copy_attributes(src, dst, excludes=None, includes=None):
+        if includes:
+            attributes = includes
+        else:
+            attributes = (
+                attr.identifier for attr in src.bl_rna.properties if not excludes or attr.identifier not in excludes
+            )
+
+        for attr in attributes:
+            if hasattr(src, attr):
+                try:
+                    setattr(dst, attr, getattr(src, attr))
+                except Exception as e:
+                    print(f"Failed to set attribute {attr}: {e}")  # TODO: Failing in some "default_value"s
+
+    dst.nodes.clear()
+    dst.links.clear()
+
+    node_mapping = {}
+    for src_node in src.nodes:
+        new_node = dst.nodes.new(src_node.bl_idname)
+        copy_attributes(src_node, new_node, excludes=EXCLUDE_FROM_NODE)
+        node_mapping[src_node] = new_node
+
+    for src_node, dst_node in node_mapping.items():
+        for i, src_input in enumerate(src_node.inputs):
+            for link in src_input.links:
+                connected_node = dst.nodes[link.from_node.name]
+                dst.links.new(connected_node.outputs[link.from_socket.name], dst_node.inputs[i])
+
+        for src_input, dst_input in zip(src_node.inputs, dst_node.inputs):
+            copy_attributes(src_input, dst_input, includes=INCLUDE_FROM_INPUT_OUTPUT)
+
+        for src_output, dst_output in zip(src_node.outputs, dst_node.outputs):
+            copy_attributes(src_output, dst_output, includes=INCLUDE_FROM_INPUT_OUTPUT)
 
 
 def is_blender_image_a_webp(image: Image) -> bool:
@@ -77,6 +135,10 @@ class Fast64Extension(GlTF2SubExtension):
 
     def post_init(self):
         self.f3d: F3D = get_F3D_GBI()
+        link_f3d_material_library()
+        mat = bpy.data.materials["fast64_f3d_material_library_beefwashere"]
+        self.base_node_tree = mat.node_tree.copy()
+        bpy.data.materials.remove(mat)
 
     def sampler_from_f3d(self, f3d_mat: F3DMaterialProperty, f3d_tex: TextureProperty):
         wrap = []
@@ -208,11 +270,13 @@ class Fast64Extension(GlTF2SubExtension):
             f3d_tex = f3d_mat.tex0 if num == "0" else f3d_mat.tex1
             self.gltf2_texture_to_f3d_texture(gltf2_texture, gltf, f3d_tex)
 
+        node_tree_copy(self.base_node_tree, blender_material.node_tree)
         blender_material.is_f3d = True
         blender_material.mat_ver = 5
 
-        # TODO: Figure out a workaround for the nodes, this will fail for now
-        # createScenePropertiesForMaterial(blender_material)
+        # TODO: Cause a reload here PLEASE
+
+        createScenePropertiesForMaterial(blender_material)
 
     def gather_import_node_after_hook(self, _vnode, gltf_node, blender_object, _gltf):
         data = self.get_gltf2_extension(gltf_node)
