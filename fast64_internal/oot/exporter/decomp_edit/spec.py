0796e3d8ecec1013e02950286160ef068ee28a85
kurethedead <epicureanistik@gmail.com>
2024-07-20 23:43:52 -0700

    Black formatting spec.py

--- fast64_internal/oot/exporter/decomp_edit/spec.py
+++ fast64_internal/oot/exporter/decomp_edit/spec.py
@@ -11,45 +11,47 @@ from collections import OrderedDict
 
 if TYPE_CHECKING:
     from ..main import SceneExport
-    
+
+
 @dataclass
 class SpecCommand:
     """This class defines a single spec command"""
 
     type: str
-    content : str = ""
-    comment : str = ""
+    content: str = ""
+    comment: str = ""
 
     def to_c(self):
         comment = f" //{self.comment}" if self.comment != "" else ""
-        
+
         # Note: This is a hacky way of handling internal preprocessor directives, which would be parsed as if they were commands.
         # This is fine as long you are not trying to modify this spec entry, and currently there are no internal preprocessor directives
         # in scene segments anyway.
-        
-        indent_string = indent if not self.type.startswith('#') else ''
+
+        indent_string = indent if not self.type.startswith("#") else ""
         content = f" {self.content.strip()}" if self.content != "" else ""
         return f"{indent_string}{self.type}{content}{comment}\n"
+
 
 @dataclass
 class SpecEntry:
     """Defines an entry of ``spec``"""
 
     commands: list[SpecCommand]
-    
+
     @staticmethod
-    def new(original : list[str]):
+    def new(original: list[str]):
         commands: list[SpecCommand] = []
         for line in original:
             comment = ""
-            if '//' in line:
-                comment = line[line.index('//') + len("//") : ]
-                line = line[:line.index('//')].strip()
+            if "//" in line:
+                comment = line[line.index("//") + len("//") :]
+                line = line[: line.index("//")].strip()
             split = line.split(" ")
             commands.append(SpecCommand(split[0], " ".join(split[1:]) if len(split) > 1 else "", comment))
-        
+
         return SpecEntry(commands)
-    
+
     def get_name(self) -> Optional[str]:
         """Returns segment name"""
         for command in self.commands:
@@ -58,35 +60,33 @@ class SpecEntry:
         return ""
 
     def to_c(self):
-        return (
-            "beginseg\n"
-            + "".join(command.to_c() for command in self.commands)
-            + "endseg"
-        )
-    
+        return "beginseg\n" + "".join(command.to_c() for command in self.commands) + "endseg"
+
+
 @dataclass
 class SpecSection:
     """Defines an 'section' of ``spec``, which is a list of segment definitions that are optionally surrounded by a preprocessor directive"""
-    
-    directive : Optional[str]
-    entries : list[SpecEntry] = field(default_factory=list)
-    
+
+    directive: Optional[str]
+    entries: list[SpecEntry] = field(default_factory=list)
+
     def to_c(self):
         directive = f"{self.directive}\n" if self.directive else ""
         terminator = "\n#endif" if self.directive and self.directive.startswith("#if") else ""
-        entry_string = '\n\n'.join(entry.to_c() for entry in self.entries)
+        entry_string = "\n\n".join(entry.to_c() for entry in self.entries)
         return f"{directive}{entry_string}{terminator}\n\n"
+
 
 @dataclass
 class SpecFile:
     """This class defines the spec's file data"""
 
-    header : str # contents of file before scene segment definitions
-    build_directory : Optional[str]
+    header: str  # contents of file before scene segment definitions
+    build_directory: Optional[str]
     sections: list[SpecSection] = field(default_factory=list)  # list of the different spec entries
 
     @staticmethod
-    def new(export_path : str):
+    def new(export_path: str):
         # read the file's data
         data = ""
         try:
@@ -94,43 +94,43 @@ class SpecFile:
                 data = file_data.read()
         except FileNotFoundError:
             raise PluginError("ERROR: Can't find spec!")
-        
+
         # Find first instance of "/assets/scenes/", indicating a scene file
         first_scene_include_index = data.index("/assets/scenes/")
         if first_scene_include_index == -1:
-            return SpecFile(data, None, []) # No scene files found - add to end
-        
+            return SpecFile(data, None, [])  # No scene files found - add to end
+
         # Get build directory, which is text right before /assets/scenes/...
         build_directory = None
         for dir in ["$(BUILD_DIR)", "build"]:
             if data[:first_scene_include_index].endswith(dir):
                 build_directory = dir
-           
-        # Go backwards up to previous "endseg" definition  
-        try:   
+
+        # Go backwards up to previous "endseg" definition
+        try:
             header_endseg_index = data[:first_scene_include_index].rfind("endseg")
         except ValueError:
             raise PluginError("endseg not found, scene segements cannot be the first segments in spec file")
-        
-        header = data[:header_endseg_index + len("endseg")]
-        
+
+        header = data[: header_endseg_index + len("endseg")]
+
         # This technically includes data after scene segments
         # However, as long as we don't have to modify them, they should be fine
-        lines = data[header_endseg_index + len("endseg"):].split("\n")
-        lines = list(filter(None, lines)) # removes empty lines
+        lines = data[header_endseg_index + len("endseg") :].split("\n")
+        lines = list(filter(None, lines))  # removes empty lines
         lines = [line.strip() for line in lines]
-        
-        sections : list[SpecSection] = []
-        current_section : Optional[SpecSection] = None
+
+        sections: list[SpecSection] = []
+        current_section: Optional[SpecSection] = None
         while len(lines) > 0:
             line = lines.pop(0)
-            if line.startswith("#if"): 
-                if current_section: # handles non-directive section preceding directive section
-                    sections.append(current_section)        
+            if line.startswith("#if"):
+                if current_section:  # handles non-directive section preceding directive section
+                    sections.append(current_section)
                 current_section = SpecSection(line)
             elif line.startswith("#endif"):
                 sections.append(current_section)
-                current_section = None # handles back-to-back directive sections
+                current_section = None  # handles back-to-back directive sections
             elif line.startswith("beginseg"):
                 if not current_section:
                     current_section = SpecSection(None)
@@ -140,28 +140,27 @@ class SpecFile:
                     segment_lines.append(next_line)
                 if len(lines) == 0:
                     raise PluginError("In spec file, a beginseg was found unterminated.")
-                lines.pop(0) # remove endseg line
+                lines.pop(0)  # remove endseg line
                 current_section.entries.append(SpecEntry.new(segment_lines))
             else:
                 # This code should ignore any other line, including comments.
                 pass
-            
-        
+
         if current_section:
-            sections.append(current_section) # add last section if non-directive    
-                
+            sections.append(current_section)  # add last section if non-directive
+
         return SpecFile(header, build_directory, sections)
-    
+
     def get_entries_flattened(self):
-        '''
-        Returns all entries as a single array, without sections. 
+        """
+        Returns all entries as a single array, without sections.
         This is a copy of the data and modifying this will not change the spec file internally.
-        '''
+        """
         return [entry for section in self.sections for entry in section.entries]
 
     def find(self, segment_name: str) -> SpecEntry:
         """Returns an entry from a segment name, returns ``None`` if nothing was found"""
-        
+
         for entry in self.get_entries_flattened():
             if entry.get_name() == segment_name:
                 return entry
@@ -169,13 +168,12 @@ class SpecFile:
 
     def append(self, entry: SpecEntry):
         """Appends an entry to the list"""
-        
+
         if len(self.sections) > 0 and self.sections[-1].directive is None:
             self.sections[-1].entries.append(entry)
         else:
             section = SpecSection(None, [entry])
             self.sections.append(section)
-            
 
     def remove(self, segment_name: str):
         """Removes an entry from a segment name"""
@@ -191,6 +189,7 @@ class SpecFile:
 
     def to_c(self):
         return f"{self.header}\n\n" + "".join(section.to_c() for section in self.sections)
+
 
 class SpecUtility:
     """This class hosts different functions to edit the spec file"""
@@ -223,8 +222,9 @@ class SpecUtility:
         segmentsToRemove: list[str] = []
         for entry in specFile.get_entries_flattened():
             # Note: you cannot do startswith(sceneName), ex. entra vs entra_n
-            if entry.get_name() == f'"{sceneName}_scene"' or \
-                re.match(f'^\"{sceneName}\_room\_[0-9]+\"$', entry.get_name()):
+            if entry.get_name() == f'"{sceneName}_scene"' or re.match(
+                f'^"{sceneName}\_room\_[0-9]+"$', entry.get_name()
+            ):
                 segmentsToRemove.append(entry.get_name())
 
         # remove the segments
@@ -262,9 +262,7 @@ class SpecUtility:
 
                 if hasSceneCS:
                     for i in range(csTotal):
-                        sceneCmds.append(
-                            SpecCommand("include", f'"{includeDir}/{sceneSegmentName}_cs_{i}.o"')
-                        )
+                        sceneCmds.append(SpecCommand("include", f'"{includeDir}/{sceneSegmentName}_cs_{i}.o"'))
 
             sceneCmds.append(SpecCommand("number", "2"))
             specFile.append(SpecEntry(sceneCmds))
