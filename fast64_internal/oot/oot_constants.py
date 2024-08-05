4d877f0d08239b398b1df517ae16a044a7e79173
Lila <lilalovesall@gmail.com>
2024-08-05 13:34:52 +0100

    Fixed issue yanis brought up

--- fast64_internal/oot/oot_constants.py
+++ fast64_internal/oot/oot_constants.py
@@ -604,12 +604,18 @@ ootEnumDrawConfig = [
 ]
 
 oot_world_defaults = {
+    "geometryMode": {
+        "zBuffer": True,
+        "shade": True,
+        "cullBack": True,
+        "lighting": True,
+        "shadeSmooth": True,
+    },
     "otherModeH": {
         "alphaDither": "G_AD_NOISE",
         "textureFilter": "G_TF_BILERP",
         "perspectiveCorrection": "G_TP_PERSP",
         "textureConvert": "G_TC_FILT",
         "cycleType": "G_CYC_2CYCLE",
-        "pipelineMode": "G_PM_NPRIMITIVE",
     },
 }
