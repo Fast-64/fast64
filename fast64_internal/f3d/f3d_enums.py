8828b38b820eb149561f046e4645789279deb5a1
Lila <lilalovesall@gmail.com>
2024-08-07 13:21:47 +0100

    Add fixed index for the ucode enum

--- fast64_internal/f3d/f3d_enums.py
+++ fast64_internal/f3d/f3d_enums.py
@@ -383,19 +383,25 @@ maxTexelCount = {
 }
 
 enumF3D = [
-    ("F3D", "F3D", "Original microcode used in SM64"),
-    ("F3DEX/LX", "F3DEX/LX", "F3DEX version 1"),
-    ("F3DLX.Rej", "F3DLX.Rej", "F3DLX.Rej"),
-    ("F3DLP.Rej", "F3DLP.Rej", "F3DLP.Rej"),
-    ("F3DEX2/LX2", "F3DEX2/LX2/ZEX", "Family of microcodes used in later N64 games including OoT"),
-    ("F3DEX2.Rej/LX2.Rej", "F3DEX2.Rej/LX2.Rej", "Variant of F3DEX2 family using vertex rejection instead of clipping"),
+    ("F3D", "F3D", "Original microcode used in SM64", 0),
+    ("F3DEX/LX", "F3DEX/LX", "F3DEX version 1", 1),
+    ("F3DLX.Rej", "F3DLX.Rej", "F3DLX.Rej", 2),
+    ("F3DLP.Rej", "F3DLP.Rej", "F3DLP.Rej", 3),
+    ("F3DEX2/LX2", "F3DEX2/LX2/ZEX", "Family of microcodes used in later N64 games including OoT", 4),
+    (
+        "F3DEX2.Rej/LX2.Rej",
+        "F3DEX2.Rej/LX2.Rej",
+        "Variant of F3DEX2 family using vertex rejection instead of clipping",
+        5,
+    ),
     (
         "F3DEX2/LX2 (Point Lit)",
         "F3DEX2/LX2 (Point Lit)",
         "Variant of F3DEX2 family with support for point lighting used in a few games including MM",
+        7,
     ),
-    ("F3DEX3", "F3DEX3", "Custom microcode by Sauraen"),
-    ("F3DZEX2 (Emu64)", "F3DZEX2 (Emu64)", "Microcode used in Animal Crossing (GC), extended version of F3DZEX"),
+    ("F3DEX3", "F3DEX3", "Custom microcode by Sauraen", 6),
+    ("F3DZEX2 (Emu64)", "F3DZEX2 (Emu64)", "Microcode used in Animal Crossing (GC), extended version of F3DZEX", 8),
 ]
 
 enumLargeEdges = [
