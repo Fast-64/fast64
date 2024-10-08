694184f60ca135dfeb7e53cba0ef5189e0d84604
MegaMech <MegaMech@users.noreply.github.com>
2024-10-08 15:29:29 -0600

    Create mk64_constants.py

---
+++ fast64_internal/mk64/mk64_constants.py
@@ -0,0 +1,20 @@
+from .data import Mk64_Data
+
+mk64Data = Mk64_Data()
+
+mk64_world_defaults = {
+    "geometryMode": {
+        "zBuffer": True,
+        "shade": True,
+        "cullBack": True,
+        "lighting": True,
+        "shadeSmooth": True,
+    },
+    "otherModeH": {
+        "alphaDither": "G_AD_NOISE",
+        "textureFilter": "G_TF_BILERP",
+        "perspectiveCorrection": "G_TP_PERSP",
+        "textureConvert": "G_TC_FILT",
+        "cycleType": "G_CYC_2CYCLE",
+    },
+}
