2841f3d8e87bb9e73eadcc4720abc75af48644bb
MegaMech <MegaMech@users.noreply.github.com>
2024-10-15 11:15:34 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -25,7 +25,6 @@ from .fast64_internal.oot.props_panel_main import OOT_ObjectProperties
 from .fast64_internal.utility_anim import utility_anim_register, utility_anim_unregister, ArmatureApplyWithMeshOperator
 
 from .fast64_internal.mk64 import MK64_Properties, mk64_register, mk64_unregister
-from .fast64_internal.mk64.mk64_constants import mk64_world_defaults
 
 from .fast64_internal.f3d.f3d_material import (
     F3D_MAT_CUR_VERSION,
