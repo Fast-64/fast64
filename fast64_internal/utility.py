c51e462990b9cea1501c57d889af9423c0dc2d3d
Lila <lilaa3public@gmail.com>
2025-02-14 20:33:28 +0000

    [SM64/F3D] Fix and update Ui Image Exporter (#452)
    * [SM64/F3D] Fix and update Ui Image Exporter
    
    Fixes #451
    Missing: Load and set commands and pallets (currently relying on multitex manager, will have to be changed somehow)
    Updated: Tex prop is now drawn using ui_image, ui_image has been updated to optionally hide low and high props (are handled by size cmd, does not exist for texrect)
    materialless_setup func for TexInfo, equivelant to moreSetupFromModel for bpy material-less contexts, obviously cannot handle flipbooks
    ignore_tex_set bool for fromProp
    Use only fromProp and updated writeAll
    Some basic ui reordering to match other panels
    
    * Fix noted issues, clean up code to be consistent, add tlut mode
    
    * use world defaults and saveModeSetting to remove revert code, remove extra syncs
    
    * close to being finished
    
    * refresh 13 memes
    
    * automatically pick 1 cycle instead of copy mode
    
    * Rasky confirmed ia16 tlut works for copy mode, cool
    
    * Revert "Rasky confirmed ia16 tlut works for copy mode, cool"
    
    This reverts commit 7fe981cc15e3a45766a2aacd21c725af8e2f708b.
    
    * [SM64/F3D] Fix and update Ui Image Exporter
    
    Updated: Tex prop is now drawn using ui_image, ui_image has been updated to optionally hide low and high props (are handled by size cmd, does not exist for texture rectangles)
    materialless_setup func for TexInfo, equivelant to moreSetupFromModel for bpy material-less contexts, obviously cannot handle flipbooks
    setup_single_tex func for TexInfo, sets up a single texture without the context of the multi texture manager
    ignore_tex_set bool for fromProp
    Use only fromProp and updated writeAll
    Use saveModeSetting (and world defaults) instead of manullay appending each DP command (except for blend color)
    Removed syncs except for the sync after the texrect cmd to do the reverts
    Removed deprecated arg for png exporting in save_textures (assumed from fimage)
    Removed the ) part of the delimiter checked when exporting "Menu", refresh 13 made it (void) instead of ()
    Some basic ui reordering to match other panels
    Added 1 cycle variant for non rgba16 exports
    Fix dynamic dls
    
    * fix dsdx and dsdy
    
    * fix overwrite data issues
    
    could not deal with aligner and palletes, now can!
    
    * sauren nitpicks
    
    * fix first test
    
    * Update f3d_texture_writer.py
    
    * make the regex allow even more whitespace
    
    * fix extra new lines

--- fast64_internal/utility.py
+++ fast64_internal/utility.py
@@ -715,7 +715,7 @@ def getExportDir(customExport, dirPath, headerType, levelName, texDir, dirName):
     return dirPath, texDir
 
 
-def overwriteData(headerRegex, name, value, filePath, writeNewBeforeString, isFunction):
+def overwriteData(headerRegex, name, value, filePath, writeNewBeforeString, isFunction, post_regex=""):
     if os.path.exists(filePath):
         dataFile = open(filePath, "r")
         data = dataFile.read()
@@ -724,7 +724,8 @@ def overwriteData(headerRegex, name, value, filePath, writeNewBeforeString, isFu
         matchResult = re.search(
             headerRegex
             + re.escape(name)
-            + ("\s*\((((?!\)).)*)\)\s*\{(((?!\}).)*)\}" if isFunction else "\[\]\s*=\s*\{(((?!;).)*);"),
+            + ("\s*\((((?!\)).)*)\)\s*\{(((?!\}).)*)\}" if isFunction else "\[\]\s*=\s*\{(((?!;).)*);")
+            + post_regex,
             data,
             re.DOTALL,
         )
