c65b4bb015ce34e5a8f53c3105fc5ddf1a6d235b
Yanis002 <35189056+Yanis002@users.noreply.github.com>
2025-04-01 13:37:36 +0200

    add a print for clarity

--- fast64_internal/oot/oot_f3d_writer.py
+++ fast64_internal/oot/oot_f3d_writer.py
@@ -352,4 +352,5 @@ def ootReadActorScale(basePath: str, overlayName: str, isLink: bool) -> Optional
             scale = scale[:-1]
         return getOOTScale(1 / float(scale))
 
+    print("WARNING: auto-detection failed, defaulting to this panel's actor scale property value")
     return None
