552bdb797cf27e3c1114217a4b01144b7d03c19b
Yanis <35189056+Yanis002@users.noreply.github.com>
2025-05-23 07:39:46 +0200

    [OoT] Add basic title cards support (#525)
    * add basic title card support
    
    * review
    
    * add z64path.h
    
    * remove added includes in favor of another PR

--- fast64_internal/oot/scene/properties.py
+++ fast64_internal/oot/scene/properties.py
@@ -309,6 +309,10 @@ class OOTSceneHeaderProperty(PropertyGroup):
         default=False,
     )
 
+    title_card_name: StringProperty(
+        name="Title Card", default="none", description="Segment name of the title card to use"
+    )
+
     def draw_props(self, layout: UILayout, dropdownLabel: str, headerIndex: int, objName: str):
         from .operators import OOT_SearchMusicSeqEnumOperator  # temp circular import fix
 
@@ -338,6 +342,9 @@ class OOTSceneHeaderProperty(PropertyGroup):
             drawEnumWithCustom(general, self, "naviCup", "Navi Hints", "")
             if headerIndex is None or headerIndex == 0:
                 self.sceneTableEntry.draw_props(general)
+                prop_split(general, self, "title_card_name", "Title Card")
+                if bpy.context.scene.ootSceneExportSettings.customExport:
+                    general.label(text="Custom Export Path enabled, title card will be ignored.", icon="INFO")
             general.prop(self, "appendNullEntrance")
 
             skyboxAndSound = layout.column()
