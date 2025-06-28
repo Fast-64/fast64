0200d3a9e9c98957ed4ba3a4e499f7bcfa7b18b7
Yanis002 <35189056+Yanis002@users.noreply.github.com>
2025-06-28 16:00:28 +0200

    comment MM game editor mode to avoid confusing users

--- __init__.py
+++ __init__.py
@@ -72,7 +72,7 @@ bl_info = {
 gameEditorEnum = (
     ("SM64", "SM64", "Super Mario 64", 0),
     ("OOT", "OOT", "Ocarina Of Time", 1),
-    ("MM", "MM (unfinished!)", "Majora's Mask", 4),
+    # ("MM", "MM", "Majora's Mask", 4),
     ("MK64", "MK64", "Mario Kart 64", 3),
     ("Homebrew", "Homebrew", "Homebrew", 2),
 )
