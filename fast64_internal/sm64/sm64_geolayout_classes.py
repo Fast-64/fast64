Fast-64/fast64

commit 98520f1d6a18477a443c430da9b1351200d03255

[SM64] Fix SM64 binary geolayout exports (#423)
* First pass

There seems to be something wrong with reverts still, will investigate

* currently hasn´t caused a bug but update this to be sure

* another similar issue

fast64_internal/sm64/sm64_geolayout_classes.py
@@ -272,8 +272,9 @@ class BaseDisplayListNode:
     bleed_independently = False  # base behavior, can be changed with obj boolProp
 
     def get_dl_address(self):
-        if self.hasDL and (self.dlRef or self.DLmicrocode is not None):
-            return self.dlRef or self.DLmicrocode.startAddress
+        assert self.dlRef is None, "dlRef not implemented in binary"
+        if self.hasDL and self.DLmicrocode is not None:
+            return self.DLmicrocode.startAddress
         return None
 
     def get_dl_name(self):
@@ -352,8 +353,8 @@ class TransformNode:
         size = self.node.size() if self.node is not None else 0
         if len(self.children) > 0 and type(self.node) in nodeGroupClasses:
             size += 8  # node open/close
-            for child in self.children:
-                size += child.size()
+        for child in self.children:
+            size += child.size()
 
         return size
 
@@ -368,11 +369,11 @@ class TransformNode:
             if type(self.node) is FunctionNode:
                 raise PluginError("An FunctionNode cannot have children.")
 
-            if data[0] in nodeGroupCmds:
+            if type(self.node) in nodeGroupClasses:
                 data.extend(bytearray([GEO_NODE_OPEN, 0x00, 0x00, 0x00]))
             for child in self.children:
                 data.extend(child.to_binary(segmentData))
-            if data[0] in nodeGroupCmds:
+            if type(self.node) in nodeGroupClasses:
                 data.extend(bytearray([GEO_NODE_CLOSE, 0x00, 0x00, 0x00]))
         elif type(self.node) is SwitchNode:
             raise PluginError("A switch bone must have at least one child bone.")
@@ -411,11 +412,11 @@ class TransformNode:
         data += "\n"
 
         if len(self.children) > 0:
-            if len(command) == 0 or command[0] in nodeGroupCmds:
+            if type(self.node) in nodeGroupClasses:
                 data += "\t" * nodeLevel + "04 00 00 00\n"
             for child in self.children:
-                data += child.toTextDump(nodeLevel + 1, segmentData)
-            if len(command) == 0 or command[0] in nodeGroupCmds:
+                data += child.toTextDump(nodeLevel + (1 if type(self.node) in nodeGroupClasses else 0), segmentData)
+            if type(self.node) in nodeGroupClasses:
                 data += "\t" * nodeLevel + "05 00 00 00\n"
         elif type(self.node) is SwitchNode:
             raise PluginError("A switch bone must have at least one child bone.")

fast64_internal/sm64/sm64_geolayout_writer.py
@@ -1534,7 +1534,8 @@ def processMesh(
 
             if len(src_meshes):
                 fMeshes = {}
-                node.dlRef = src_meshes[0]["name"]
+                if useGeoEmpty:
+                    node.dlRef = src_meshes[0]["name"]
                 node.drawLayer = src_meshes[0]["layer"]
                 processed_inline_geo = True
 
@@ -1559,7 +1560,8 @@ def processMesh(
                     temp_obj["src_meshes"] = [
                         ({"name": fMesh.draw.name, "layer": drawLayer}) for drawLayer, fMesh in fMeshes.items()
                     ]
-                    node.dlRef = temp_obj["src_meshes"][0]["name"]
+                    if useGeoEmpty:
+                        node.dlRef = temp_obj["src_meshes"][0]["name"]
                 else:
                     # TODO: Display warning to the user that there is an object that doesn't have polygons
                     print("Object", obj.original_name, "does not have any polygons.")

fast64_internal/f3d/f3d_gbi.py
@@ -2969,14 +2969,17 @@ class FTriGroup:
         return self.triList.get_ptr_addresses(f3d)
 
     def set_addr(self, startAddress, f3d):
-        addrRange = self.triList.set_addr(startAddress, f3d)
+        addrRange = (startAddress, startAddress)
+        if self.triList.tag.Export:
+            addrRange = self.triList.set_addr(startAddress, f3d)
         addrRange = self.vertexList.set_addr(addrRange[1])
         return startAddress, addrRange[1]
 
     def save_binary(self, romfile, f3d, segments):
         for celTriList in self.celTriLists:
             celTriList.save_binary(romfile, f3d, segments)
-        self.triList.save_binary(romfile, f3d, segments)
+        if self.triList.tag.Export:
+            self.triList.save_binary(romfile, f3d, segments)
         self.vertexList.save_binary(romfile)
 
     def to_c(self, f3d, gfxFormatter):
@@ -3083,15 +3086,17 @@ class FMaterial:
         return addresses
 
     def set_addr(self, startAddress, f3d):
-        addrRange = self.material.set_addr(startAddress, f3d)
-        startAddress = addrRange[0]
-        if self.revert is not None:
+        addrRange = (startAddress, startAddress)
+        if self.material.tag.Export:
+            addrRange = self.material.set_addr(addrRange[1], f3d)
+        if self.revert is not None and self.revert.tag.Export:
             addrRange = self.revert.set_addr(addrRange[1], f3d)
         return startAddress, addrRange[1]
 
     def save_binary(self, romfile, f3d, segments):
-        self.material.save_binary(romfile, f3d, segments)
-        if self.revert is not None:
+        if self.material.tag.Export:
+            self.material.save_binary(romfile, f3d, segments)
+        if self.revert is not None and self.revert.tag.Export:
             self.revert.save_binary(romfile, f3d, segments)
 
     def to_c(self, f3d):

commit b588cd991a12ea1db97e2c8df3c29ce584a86875

[SM64] Fix mesh instancing, regression from #423

fast64_internal/sm64/sm64_geolayout_classes.py
@@ -284,14 +284,19 @@ class BaseDisplayListNode:
 
     dl_ext = "WITH_DL"  # add dl_ext to geo command if command has a displaylist
     override_layer = False
+    dlRef: str | GfxList | None
 
     def get_dl_address(self):
-        assert self.dlRef is None, "dlRef not implemented in binary"
+        assert not isinstance(self.dlRef, str), "dlRef string not supported in binary"
+        if isinstance(self.dlRef, GfxList):
+            return self.dlRef.startAddress
         if self.hasDL and self.DLmicrocode is not None:
             return self.DLmicrocode.startAddress
         return None
 
     def get_dl_name(self):
+        if isinstance(self.dlRef, GfxList):
+            return self.dlRef.name
         if self.hasDL and (self.dlRef or self.DLmicrocode is not None):
             return self.dlRef or self.DLmicrocode.name
         return "NULL"

fast64_internal/sm64/sm64_geolayout_writer.py
@@ -1504,16 +1504,22 @@ def processMesh(
 
             if len(src_meshes):
                 fMeshes = {}
-                if useGeoEmpty:
-                    node.dlRef = src_meshes[0]["name"]
+                # find dl
+                draw, name = None, src_meshes[0]["dl"]
+                for fmesh in fModel.meshes.values():
+                    for fmesh_draw in [fmesh.draw] + fmesh.draw_overrides:
+                        if fmesh_draw.name == name:
+                            draw = fmesh_draw
+                            break
+                node.dlRef = draw
                 node.drawLayer = src_meshes[0]["layer"]
                 processed_inline_geo = True
 
                 for src_mesh in src_meshes[1:]:
                     additionalNode = (
-                        DisplayListNode(src_mesh["layer"], src_mesh["name"])
+                        DisplayListNode(src_mesh["layer"], src_mesh["dl"])
                         if not isinstance(node, BillboardNode)
-                        else BillboardNode(src_mesh["layer"], True, [0, 0, 0], src_mesh["name"])
+                        else BillboardNode(src_mesh["layer"], True, [0, 0, 0], src_mesh["dl"])
                     )
                     additionalTransformNode = TransformNode(additionalNode)
                     transformNode.children.append(additionalTransformNode)
@@ -1531,10 +1537,9 @@ def processMesh(
                 )
                 if fMeshes:
                     temp_obj["src_meshes"] = [
-                        ({"name": fMesh.draw.name, "layer": drawLayer}) for drawLayer, fMesh in fMeshes.items()
+                        ({"dl": fMesh.draw.name, "layer": drawLayer}) for drawLayer, fMesh in fMeshes.items()
                     ]
-                    if useGeoEmpty:
-                        node.dlRef = temp_obj["src_meshes"][0]["name"]
+                    node.dlRef = temp_obj["src_meshes"][0]["dl"]
                 else:
                     # TODO: Display warning to the user that there is an object that doesn't have polygons
                     print("Object", obj.original_name, "does not have any polygons.")