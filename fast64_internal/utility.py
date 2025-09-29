a4a1e7afdae972711196cd1d15f6295f08746288
Lila <lilaa3public@gmail.com>
2025-09-29 23:22:10 +0100

    [Codebase] Fix setOrigin AGAIN
    transform_apply was not neeeded, typehint was missing and offset was being affected by obj.location, we actually just zero it out now before applying the world matrix.
    also removed the temp_override cause transform already does that.

--- fast64_internal/utility.py
+++ fast64_internal/utility.py
@@ -713,21 +713,21 @@ def checkIdentityRotation(obj, rotation, allowYaw):
 
 def setOrigin(obj: bpy.types.Object, target_loc: mathutils.Vector):
     assert obj.type == "MESH", "Object is not a mesh"
+    mesh: bpy.types.Mesh = obj.data
 
     if not target_loc.is_frozen:
         target_loc = target_loc.copy()
-    offset = target_loc - obj.location
-    with bpy.context.temp_override(
-        selected_objects=[obj],
-        active_object=obj,
-    ):
-        obj.data.transform(mathutils.Matrix.Translation(-offset))
-        # Applying location puts the object origin at world origin
-        # (It is only needed to apply location to set the origin,
-        #  but historically this function has applied all transforms
-        #  so just keep doing that to not break anything)
-        bpy.ops.object.transform_apply()
-        obj.location = target_loc
+    mat = obj.matrix_world.copy()
+
+    # Applying location puts the object origin at world origin
+    # (It is only needed to apply location to set the origin,
+    #  but historically this function has applied all transforms
+    #  so just keep doing that to not break anything)
+    obj.matrix_world.identity()
+    mesh.transform(mat)
+
+    mesh.transform(Matrix.Translation(-target_loc))
+    obj.location = target_loc
 
 
 def checkIfPathExists(filePath):
