bd952a81e7b938c95258cf46a4dbbc4029247b9f
Yanis <35189056+Yanis002@users.noreply.github.com>
2026-03-14 20:47:44 +0100

    [OoT] Fixed DL Exporter using the wrong filename (#640)
    * fixed 617
    
    * review

--- fast64_internal/z64/f3d/operators.py
+++ fast64_internal/z64/f3d/operators.py
@@ -51,7 +51,13 @@ def ootConvertMeshToC(
     exportPath = bpy.path.abspath(settings.customPath)
     isCustomExport = settings.isCustom
     removeVanillaData = settings.removeVanillaData
-    name = toAlnum(originalObj.name)
+
+    obj_name = toAlnum(originalObj.name)
+    assert obj_name is not None
+
+    filename = toAlnum(settings.filename) if settings.isCustomFilename else obj_name
+    assert filename is not None
+
     overlayName = settings.actorOverlayName
     flipbookUses2DArray = settings.flipbookUses2DArray
     flipbookArrayIndex2D = settings.flipbookArrayIndex2D if flipbookUses2DArray else None
@@ -59,7 +65,7 @@ def ootConvertMeshToC(
     try:
         obj, allObjs = ootDuplicateHierarchy(originalObj, None, False, OOTObjectCategorizer())
 
-        fModel = OOTModel(name, DLFormat, None)
+        fModel = OOTModel(obj_name, DLFormat, None)
         triConverterInfo = TriangleConverterInfo(obj, None, fModel.f3d, finalTransform, getInfoDict(obj))
         fMeshes = saveStaticModel(
             triConverterInfo, fModel, obj, finalTransform, fModel.name, not saveTextures, False, "oot"
@@ -67,7 +73,7 @@ def ootConvertMeshToC(
 
         # Since we provide a draw layer override, there should only be one fMesh.
         for fMesh in fMeshes.values():
-            fMesh.draw.name = name
+            fMesh.draw.name = obj_name
 
         ootCleanupScene(originalObj, allObjs)
 
@@ -75,7 +81,6 @@ def ootConvertMeshToC(
         ootCleanupScene(originalObj, allObjs)
         raise Exception(str(e))
 
-    filename = settings.filename if settings.isCustomFilename else name
     data = CData()
     data.header = f"#ifndef {filename.upper()}_H\n" + f"#define {filename.upper()}_H\n\n" + '#include "ultra64.h"\n'
 
@@ -105,11 +110,11 @@ def ootConvertMeshToC(
 
     if not isCustomExport:
         writeTextureArraysExisting(bpy.context.scene.ootDecompPath, overlayName, False, flipbookArrayIndex2D, fModel)
-        addIncludeFiles(folderName, path, name)
+        addIncludeFiles(folderName, path, filename)
         if removeVanillaData:
             headerPath = os.path.join(path, folderName + ".h")
             sourcePath = os.path.join(path, folderName + ".c")
-            removeDL(sourcePath, headerPath, name)
+            removeDL(sourcePath, headerPath, filename)
 
 
 class OOT_ImportDL(Operator):
